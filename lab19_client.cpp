6cfe15ca686029c4ab9e736402ee847743d3f99b
Ilya <vinnikov_i@mail.ru>
2021-12-25 20:51:48 +0300

    Lab 18-19

---
+++ lab19_client.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include "mpi.h"
+int main(int argc, char **argv)
+{
+	int got, send = 42;
+	MPI_Init(&argc, &argv);
+	char port_name[MPI_MAX_PORT_NAME];
+	MPI_Status status;
+	MPI_Comm intercomm;
+	strcpy(port_name, argv[1]);
+	printf("Attempt to connect \n");
+	MPI_Comm_connect(port_name, MPI_INFO_NULL, 0, MPI_COMM_SELF, &intercomm);
+	printf("Server connection \n");
+	MPI_Send(&send, 1, MPI_INT, 0, 0, intercomm);
+	printf("Client sent value: %d\n", send);
+	MPI_Recv(&got, 1, MPI_INT, 0, 0, intercomm, &status);
+	printf("Client got value: %d\n", got);
+	MPI_Finalize();
+	return 0;
+}
